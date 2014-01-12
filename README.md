f24b95abc4104e4ea8912d1a927221c001646f66
Eric Lachenmeier <elachenmeier@yahoo.com>
2014-01-12 14:53:09 -0800

    new resume

---
+++ README.md
@@ -0,0 +1,9 @@
+Hi, Welcome to Eric Lachenmeier's demo Sample App Home Page
+
+This App is live on Heroku: http://ericlachenmeier.herokuapp.com
+
+If you would like to contact me, please do:
+
+eric.lachenmeier@gmail.com
+
+(650) 283-8835
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'aed63ce04d012e46cdd7b29e222d4af5f3633b41a878fe426c05698a0cfb68ec86d86967f2aaa303376d4eb5d81045bcf4eb25524cb513b54a71de6f3ce156c1'
+SampleApp::Application.config.secret_token = 'e3bada94f9de7bbaf8d2ac710d3669f466860b6d755ae866314fcce7b542f342816eebe6dac273c925e33c1244c324ca09fce7800fb437e2e2ad41d62404698d'
